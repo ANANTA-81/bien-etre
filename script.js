9e8bf1a88aa85f58a33ac1891fe0674798149229
ANANTA-81 <positive.ananta@gmail.com>
2026-03-12 14:46:45 +0100

    Add toggleDetails function for displaying details

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const mobileMenu = document.querySelector('.mobile-menu');
 burger.addEventListener('click', () => {
     mobileMenu.classList.toggle('open');
 });
+function toggleDetails(id) {
+    const block = document.getElementById('details-' + id);
+    block.style.display = block.style.display === 'block' ? 'none' : 'block';
+}
